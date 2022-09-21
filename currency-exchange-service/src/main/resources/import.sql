48d495de6d690af23828318abd25abc7dd834f35
Divyansh  Singh <divyanshsinghbais@hotmail.com>
2022-09-21 13:45:58 +0530

    up

--- currency-exchange-service/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO exchange_value (id,currency_from,currency_to,conversion_mulitple,port) VALUES (10001,'USD','INR',70,0);
-INSERT INTO exchange_value (id,currency_from,currency_to,conversion_mulitple,port) VALUES (10002,'EUR','INR',112,0);
-INSERT INTO exchange_value (id,currency_from,currency_to,conversion_mulitple,port) VALUES (10003,'AUD','INR',25,0);
